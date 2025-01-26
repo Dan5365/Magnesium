79bc9a0f5015c08f718082e02e8b9d7d4e599690
Dan <240754@astanait.edu.kz>
2025-01-26 16:47:47 +0500

    Удалены старые файлы, добавлены новые

--- serving-web-content/src/main/java/com/example/serving_web_content/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.serving_web_content.repository;
-
-import com.example.serving_web_content.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
