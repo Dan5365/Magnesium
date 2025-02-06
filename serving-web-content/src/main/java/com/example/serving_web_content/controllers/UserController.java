7f127a95479dced457d38f6050881e530b5b5783
zhannakabdullina <kabdullina07@icloud.com>
2025-02-06 11:04:35 +0500

    data validation

--- serving-web-content/src/main/java/com/example/serving_web_content/controllers/UserController.java
+++ serving-web-content/src/main/java/com/example/serving_web_content/controllers/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private UserService userService;
+     private UserService userService;
 
     //Add user REST API
     @PostMapping
